c9f67bc8ac8b4820daeaa35035ead1832a3d241e
nanan10 <76104854+nanan10@users.noreply.github.com>
2021-05-20 13:59:25 +0100

    question 14

--- src/main/scala/ExamenBigData.scala
+++ src/main/scala/ExamenBigData.scala
@@ -1,12 +1,29 @@
 object ExamenBigData {
   def main(args: Array[String]): Unit = {
+    val resultat= List("julien","paul", "jean","rac", "trec", "joel", "ed", "chris", "maurice")
 
-
+    println(listefin_n(resultat))
   }
 
   def listefin_n(liste1: List[String]): List[String] = {
     val liste2= liste1.filter(s => (s.charAt(s.length-1) == 'n'))
-
     return liste2
   }
+  def liste_double():Unit={
+    val  Maliste = List(List("ecommercemag.fr"," "),List("https://www.journalducm.com/contact/","Payant"),
+      List("https://www.zatsaz.com/",""),List("https://www.lerevenu.com",""),
+      List("https://www.cadre-dirigfdeant-magazine.com/","payant"),
+      List("https://www.silicon.fr/services/contact#annoncer/","Payant"),
+      List("https://www.channelbiz.fr/nous-contacter/",""),
+      List("https://www.itespresso.fr/",""),
+      List("https://www.industrie-mag.com/article4.html","invite"),
+      List("https://www.jesuisundev.com/article-invite/","invite"),
+      List("https://www.numerama.com/",""))
+    Maliste.foreach(l=>{println(l(0))})
+
+ }
+
+
+
+
 }
